1c773037d613ecb5b7a3c8424b9040b73db2cb03
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2024-02-12 11:17:33 -0500

    imports

--- WeatherApp/WeatherApp_Model.py
+++ WeatherApp/WeatherApp_Model.py
@@ -1,2 +1,10 @@
 import requests
+import json
 
+weather = requests.get('https://w3schools.com/python/demopage.htm')
+
+def get_weather():
+    pass 
+
+def get_location():
+    pass
---
+++ WeatherApp/WeatherApp_View.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import datetime
+from PIL import ImageTk
+from PIL import Image
+
+root = tk()
+
+root.title("Welcome to the geather App")
+root.geometry('350x200')
+
+# All widgets are here
+
+# Excecute tkinter
+root.mainloop()
