1b87ba56f49357de1459a6dab0063936e3bdef0f
Juan Martin Santos Ayala <78159223+JMarts32@users.noreply.github.com>
2024-02-15 10:31:29 -0500

    Templete finished

---
+++ automate_ftp/ftp.py
@@ -0,0 +1,19 @@
+"""
+Automate the process of transfering the files from an external source
+to the companies internal network
+"""
+
+import ftplib
+import os
+import shutil
+import sched
+
+# Set up the log file to undrstand the files that were tranferred success
+
+# Get the connection to the external network
+
+# Iterate trhough the files and download them into the local
+
+# Move the files from the local directory to the internal network
+
+# Schedule the script to run everyday
